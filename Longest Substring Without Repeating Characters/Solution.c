872e5bfb6e2988a265351109fc1adfd6c70d7981
Irony-J <jztirony@gmail.com>
2017-07-14 15:07:57 +1000

    c solution

--- Longest Substring Without Repeating Characters/Solution.c
+++ Longest Substring Without Repeating Characters/Solution.c
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <stdlib.h>
+
 #define min(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
@@ -6,4 +9,28 @@
 
 int lengthOfLongestSubstring(char* s) {
     
+	if(s==NULL) return 0;
+
+	int len = strlen(s);
+	if(len == 0) return 0;
+	if(len == 1) return 1;
+
+	int* max = (int*)malloc(sizeof(int)*len);
+	max[len - 1] = 1;
+	int P[128];
+	P[s[len-1]] = len - 1;
+	int result = -1;
+
+	for(int i = len -2; i>=0; i--){
+		max[i] = (P[s[i]]==0) ? (max[i+1] + 1) : (min(P[s[i]] - i, max[i+1]+1));
+		if(result < max[i])
+			result = max[i];
+		P[s[i]] = i;
+	}
+
+	return result;
+}
+
+int main(){
+	
 }
