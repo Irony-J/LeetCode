8df7df483399d3448db7d3176b3da9ff5c89bf4d
Irony-J <jztirony@gmail.com>
2017-08-02 22:24:07 +1000

    DP

---
+++ Longest Palindromic Substring/Palindromic_DP.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+char* longestPalindrome(char* s) {
+    
+    int len = strlen(s);
+    int dp[len][len];
+    
+    return s;
+
+}
+
+int main(int argc,char* argv[]){
+
+	printf("Result:%s\n",longestPalindrome(argv[1]));
+}
